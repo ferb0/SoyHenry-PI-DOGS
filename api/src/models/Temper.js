ddc93ace4959c05650957de2f1eef4bfc2919f50
ferb0 <ferb@e.email>
2022-10-22 17:40:03 -0300

    Config básica de la Base Datos.

--- api/src/models/Temper.js
+++ api/src/models/Temper.js
@@ -1,36 +1,17 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
-
-// Librería de NODE.
-const crypto = require("crypto");
-
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Dog', {
+  sequelize.define('Temper', {
     id: {
       type: DataTypes.UUID,
       primaryKey: true,
     },
-
+    
     name: {
       type: DataTypes.STRING,
       allowNull: false
-    },
-
-    height: {
-      type: DataTypes.FLOAT,
-      allowNull: false
-    },
-
-    weight: {
-      type: DataTypes.FLOAT,
-      allowNull: false
-    },
-
-    lifeSpan: {
-      type: DataTypes.INTEGER,
-      allowNull: true
     }
   },
     { timestamps: false }
