f5cc126f845583a7c047beb7926d2626e507f1e8
FerB <ferb@e.email>
2022-10-28 09:26:52 -0300

    Se arma mensaje cuanso no hay resultados.

--- client/src/componets/ConjuntBreed/ConjuntBreed.jsx
+++ client/src/componets/ConjuntBreed/ConjuntBreed.jsx
@@ -22,31 +22,37 @@ export default function ConjuntBreed() {
         temperFilter = breeds;
     }
 
+    // Se filtra por origen
+    let temperSource = temperFilter?.map(el => {
+        if (filterSource === ALL)
+            return <SummaryBredd
+                key={el.id}
+                id={el.id}
+                name={el.name}
+                weight={el.weight}
+                img={el.img}
+                temper={el.temper}
+            />
+
+        else if (el.source === filterSource)
+            return <SummaryBredd
+                key={el.id}
+                id={el.id}
+                name={el.name}
+                weight={el.weight}
+                img={el.img}
+                temper={el.temper}
+            />
+    })
+
     // Contenplar la condicion de que sea ceros elementos a mistrar con o son filtro
     return (
         <div className="ConjuntBreed">
             <h1>Conjunt Breed</h1>
-            {temperFilter?.map(el => {
-                if (filterSource === ALL)
-                    return <SummaryBredd
-                        key={el.id}
-                        id={el.id}
-                        name={el.name}
-                        weight={el.weight}
-                        img={el.img}
-                        temper={el.temper}
-                    />
-
-                else if (el.source === filterSource)
-                    return <SummaryBredd
-                        key={el.id}
-                        id={el.id}
-                        name={el.name}
-                        weight={el.weight}
-                        img={el.img}
-                        temper={el.temper}
-                    />
-            })}
+            <br/>
+            {temperSource.length === 0 ?
+                <p>Sin resultados.</p> :
+                temperSource}
         </div>
     );
 };
