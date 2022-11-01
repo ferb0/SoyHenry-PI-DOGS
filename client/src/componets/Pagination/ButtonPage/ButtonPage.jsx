4c333bd73c3e0edc84bce5ce8fbc851415e01927
FerB <ferb@e.email>
2022-11-01 10:12:12 -0300

    Se continua con paginación.

--- client/src/componets/Pagination/ButtonPage/ButtonPage.jsx
+++ client/src/componets/Pagination/ButtonPage/ButtonPage.jsx
@@ -1,5 +1,5 @@
 export default function ButtonPage(props) {
     return (
-        <button value={props.value} onClick={props.handleClick}>{props.value}</button>
+        <button value={props.value} onClick={props.handleClick} disabled={props.disable}>{props.value}</button>
     )
 };
--- client/src/componets/Pagination/Pagination.jsx
+++ client/src/componets/Pagination/Pagination.jsx
@@ -13,7 +13,7 @@ export default function Pagination() {
             temperSelected: state.temperSelected
         }
     });
-    let [breedsFinal, setBreedsFinal] = useState([0, []]);
+    let [breedsFinal, setBreedsFinal] = useState([0, 0, []]);
 
     // let temperFiltered = temperFilter(breeds, temperSelected);
     // let breedsFiltered = sourceFilter(temperFiltered, sourceSelected);
@@ -21,25 +21,26 @@ export default function Pagination() {
     useEffect(() => {
         let temperFiltered = temperFilter(breeds, temperSelected);
         let breedsFiltered = sourceFilter(temperFiltered, sourceSelected);
-        setBreedsFinal([breedsFiltered.length, cantFilter(breedsFiltered)]);
+        setBreedsFinal([1, breedsFiltered.length, cantFilter(breedsFiltered)]);
     }, [breeds, sourceSelected, temperSelected]);
 
     function handleClick(e) {
         let temperFiltered = temperFilter(breeds, temperSelected);
         let breedsFiltered = sourceFilter(temperFiltered, sourceSelected);
-        setBreedsFinal([breedsFiltered.length, cantFilter(breedsFiltered, e.target.value)]);
+        setBreedsFinal([parseInt(e.target.value), breedsFiltered.length, cantFilter(breedsFiltered, e.target.value)]);
     };
 
     let pages = [];
-    for (let i = 0; i < Math.ceil(breedsFinal[0] / 8); i++) {
-        pages.push(<ButtonPage key={i+1} handleClick={handleClick} value={i + 1} />);
+    console.log(breedsFinal)
+    for (let i = 0; i < Math.ceil(breedsFinal[1] / 8); i++) {
+        pages.push(<ButtonPage key={i+1} handleClick={handleClick} value={i + 1} disable={i === breedsFinal[0]-1}/>);
     };
 
     return (
         <div>
             <h3>Pagination</h3>
             {pages}
-            <ConjuntBreed breeds={breedsFinal[1]} />
+            <ConjuntBreed breeds={breedsFinal[2]} />
         </div>
     )
 };
