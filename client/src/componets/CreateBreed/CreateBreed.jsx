2dae1e0068a0fb13f1570d3666754ea4a16f31f7
ferb0 <ferb@e.email>
2022-11-03 21:24:46 -0300

    Estilos con imagen.

--- client/src/componets/CreateBreed/CreateBreed.jsx
+++ client/src/componets/CreateBreed/CreateBreed.jsx
@@ -3,6 +3,9 @@ import checker from "./checker.js";
 
 import formatData from "./formatData.js";
 
+import s from './CreateBreed.module.css';
+import imageCreate from './images/createBreed.webp';
+
 export default function Createbreed() {
   const [input, setInput] = React.useState({
     name: "",
@@ -60,43 +63,68 @@ export default function Createbreed() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Nombre: </label>
-      <input type='text' name="name" onChange={handleOnChange}></input>
-      {error.name ? <p>Mal formato</p> : null}
-      <br />
-      <label>Altura mínima: </label>
-      <input type='text' name="minHeight" onChange={handleOnChange}></input>
-      {error.minHeight ? <p>Mal formato</p> : null}
-      <br />
-      <label>Altura máxima: </label>
-      <input type='text' name="maxHeight" onChange={handleOnChange}></input>
-      {error.maxHeight ? <p>Mal formato</p> : null}
-      <br />
-      <label>Peso Mínimo: </label>
-      <input type='text' name="minWeight" onChange={handleOnChange}></input>
-      {error.minWeight ? <p>Mal formato</p> : null}
-      <br />
-      <label>Peso máximo: </label>
-      <input type='text' name="maxWeight" onChange={handleOnChange}></input>
-      {error.maxWeight ? <p>Mal formato</p> : null}
-      <br />
-      <label>Años de vida mínimos: </label>
-      <input type='text' name="minLifeSpan" onChange={handleOnChange}></input>
-      {error.minLifeSpan ? <p>Mal formato</p> : null}
-      <br />
-      <label>Años de vida máximos: </label>
-      <input type='text' name="maxLifeSpan" onChange={handleOnChange}></input>
-      {error.maxLifeSpan ? <p>Mal formato</p> : null}
-      <br />
-      <label>Temperamentos: </label>
-      <input type='text' name="temper" onChange={handleOnChange}></input>
-      {error.temper ? <p>Mal formato</p> : null}
+    <div className={`${s.Create} Global`}>
+      <img className={`${s.Create}`} src={imageCreate} alt="imagen" width={`300px`} />
+      <form className={`${s.form}`} onSubmit={handleSubmit}>
 
-      <p>Agregar separados por comas</p>
-      <br />
-      <button type="submit">Crear Raza</button>
-      {send === undefined ? null : send ? <p>Datos enviados</p> : <p>Datos no enviados</p>}
-    </form>
+        <div className={`${s.elementForm}`}>
+          <label className={`${s.label}`}>Nombre: </label>
+          <input type='text' name="name" size="20" onChange={handleOnChange}></input>
+          {error.name ? <p>Mal formato</p> : null}
+        </div>
+
+
+        <div className={`${s.elementForm}`}>
+          <label className={`${s.label}`}>Altura mínima: </label>
+          <input type='text' name="minHeight" size="20" onChange={handleOnChange}></input>
+          {error.minHeight ? <p>Mal formato</p> : null}
+        </div>
+
+
+        <div className={`${s.elementForm}`}>
+          <label className={`${s.label}`}>Altura máxima: </label>
+          <input type='text' name="maxHeight" size="20" onChange={handleOnChange}></input>
+          {error.maxHeight ? <p>Mal formato</p> : null}
+        </div>
+
+
+        <div className={`${s.elementForm}`}>
+          <label className={`${s.label}`}>Peso Mínimo: </label>
+          <input type='text' name="minWeight" size="20" onChange={handleOnChange}></input>
+          {error.minWeight ? <p>Mal formato</p> : null}
+        </div>
+
+
+        <div className={`${s.elementForm}`}>
+          <label className={`${s.label}`}>Peso máximo: </label>
+          <input type='text' name="maxWeight" size="20" onChange={handleOnChange}></input>
+          {error.maxWeight ? <p>Mal formato</p> : null}
+        </div>
+
+
+        <div className={`${s.elementForm}`}>
+          <label className={`${s.label}`}>Años de vida mínimos: </label>
+          <input type='text' name="minLifeSpan" size="20" onChange={handleOnChange}></input>
+          {error.minLifeSpan ? <p>Mal formato</p> : null}
+        </div>
+
+        <div className={`${s.elementForm}`}>
+          <label className={`${s.label}`}>Años de vida máximos: </label>
+          <input type='text' name="maxLifeSpan" size="20" onChange={handleOnChange}></input>
+          {error.maxLifeSpan ? <p>Mal formato</p> : null}
+        </div>
+
+
+        <div className={`${s.elementForm}`}>
+          <label className={`${s.label}`}>Temperamentos: </label>
+          <input type='text' name="temper" size="20" onChange={handleOnChange}></input>
+          {error.temper ? <p>Mal formato</p> : null}
+          <p>Agregar separados por comas</p>
+        </div>
+
+        <button type="submit">Crear Raza</button>
+        {send === undefined ? null : send ? <p>Datos enviados</p> : <p>Datos no enviados</p>}
+      </form>
+    </div>
   )
 };
