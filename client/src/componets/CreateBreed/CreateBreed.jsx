5fd91e6aa984e7356d0275ac910e6fc28666cff0
FerB <ferb@e.email>
2022-11-04 11:50:56 -0300

    Solo agregan detalles en formatos de mensajes y controles.

--- client/src/componets/CreateBreed/CreateBreed.jsx
+++ client/src/componets/CreateBreed/CreateBreed.jsx
@@ -30,11 +30,12 @@ export default function Createbreed() {
   });
 
   const [send, setSend] = React.useState(undefined);
+  const [errorDataToSend, setErrorDataToSend] = React.useState(undefined);
 
   function handleOnChange(event) {
     setError({
       ...error,
-      [event.target.name]: checker(event.target.name, event.target.value)
+      [event.target.name]: checker(event.target.name, event.target.value, input)
     });
 
     setInput({
@@ -47,83 +48,91 @@ export default function Createbreed() {
     e.preventDefault();
     let data = formatData(input);
 
-    fetch(`http://localhost:3001/dogs`,
-      {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: { "Content-type": "application/json; charset=UTF-8" }
-      })
-      .then(response => response.json())
-      .then((response) => {
-        if (response.hasOwnProperty('msg'))
-          setSend(true);
-        if (response.hasOwnProperty('err'))
-          setSend(false);
-      })
+    if (data) {
+      setErrorDataToSend(false);
+      fetch(`http://localhost:3001/dogs`,
+        {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: { "Content-type": "application/json; charset=UTF-8" }
+        })
+        .then(response => response.json())
+        .then((response) => {
+          if (response.hasOwnProperty('msg'))
+            setSend(true);
+          if (response.hasOwnProperty('err'))
+            setSend(false);
+        })
+    }
+    else
+      setErrorDataToSend(true);
   };
 
   return (
     <div className={`${s.Create} Global`}>
-      <img className={`${s.Create}`} src={imageCreate} alt="imagen" width={`300px`} />
+      <img className={`${s.img}`} src={imageCreate} alt="imagen" width={`300px`} />
+
       <form className={`${s.form}`} onSubmit={handleSubmit}>
 
         <div className={`${s.elementForm}`}>
           <label className={`${s.label}`}>Nombre: </label>
           <input type='text' name="name" size="20" onChange={handleOnChange}></input>
-          {error.name ? <p>Mal formato</p> : null}
+          {error.name ? <p className={`${s.msgError} Global`}>Formato inadecuado.</p> : null}
         </div>
-
 
         <div className={`${s.elementForm}`}>
           <label className={`${s.label}`}>Altura mínima: </label>
           <input type='text' name="minHeight" size="20" onChange={handleOnChange}></input>
-          {error.minHeight ? <p>Mal formato</p> : null}
+          {error.minHeight ? <p className={`${s.msgError} Global`}>Formato inadecuado.</p> : null}
         </div>
-
 
         <div className={`${s.elementForm}`}>
           <label className={`${s.label}`}>Altura máxima: </label>
           <input type='text' name="maxHeight" size="20" onChange={handleOnChange}></input>
-          {error.maxHeight ? <p>Mal formato</p> : null}
+          {error.maxHeight ? <p className={`${s.msgError} Global`}>Formato inadecuado.</p> : null}
         </div>
-
 
         <div className={`${s.elementForm}`}>
           <label className={`${s.label}`}>Peso Mínimo: </label>
           <input type='text' name="minWeight" size="20" onChange={handleOnChange}></input>
-          {error.minWeight ? <p>Mal formato</p> : null}
+          {error.minWeight ? <p className={`${s.msgError} Global`}>Formato inadecuado.</p> : null}
         </div>
-
 
         <div className={`${s.elementForm}`}>
           <label className={`${s.label}`}>Peso máximo: </label>
           <input type='text' name="maxWeight" size="20" onChange={handleOnChange}></input>
-          {error.maxWeight ? <p>Mal formato</p> : null}
+          {error.maxWeight ? <p className={`${s.msgError} Global`}>Formato inadecuado.</p> : null}
         </div>
-
 
         <div className={`${s.elementForm}`}>
           <label className={`${s.label}`}>Años de vida mínimos: </label>
           <input type='text' name="minLifeSpan" size="20" onChange={handleOnChange}></input>
-          {error.minLifeSpan ? <p>Mal formato</p> : null}
+          {error.minLifeSpan ? <p className={`${s.msgError} Global`}>Formato inadecuado.</p> : null}
         </div>
 
         <div className={`${s.elementForm}`}>
           <label className={`${s.label}`}>Años de vida máximos: </label>
           <input type='text' name="maxLifeSpan" size="20" onChange={handleOnChange}></input>
-          {error.maxLifeSpan ? <p>Mal formato</p> : null}
+          {error.maxLifeSpan ? <p className={`${s.msgError} Global`}>Formato inadecuado.</p> : null}
         </div>
-
 
         <div className={`${s.elementForm}`}>
           <label className={`${s.label}`}>Temperamentos: </label>
           <input type='text' name="temper" size="20" onChange={handleOnChange}></input>
-          {error.temper ? <p>Mal formato</p> : null}
-          <p>Agregar separados por comas</p>
+          {error.temper ? <p className={`${s.msgError} Global`}>Formato inadecuado.</p> : null}
+          <p className={`${s.msgDetail}`}>(Agregar temperamentos<br/>separados por comas.)</p>
         </div>
 
         <button type="submit">Crear Raza</button>
-        {send === undefined ? null : send ? <p>Datos enviados</p> : <p>Datos no enviados</p>}
+        {send === undefined ?
+          null
+          :
+          send ?
+            <p>Datos enviados</p>
+            :
+            <p className={`${s.msgError} Global`}>Datos no cargados.</p>}
+
+        {errorDataToSend ? <p className={`${s.msgError} Global`}>Faltan datos.</p> : null}
       </form>
     </div>
   )
--- client/src/componets/CreateBreed/formatData.js
+++ client/src/componets/CreateBreed/formatData.js
@@ -1,8 +1,12 @@
 export default function formatData(input) {
     // Se le da formato para enviar.
 
+    if (!input.name || !input.minHeight || !input.maxHeight || !input.minWeight || !input.maxWeight || !input.minLifeSpan || !input.maxLifeSpan || !input.temper){
+        return false;
+    }
+
     //Para quitar temper vacios
-    let temper = input.temper.replace(/\s/g, '').split(',').filter(el => el !== "");
+    let temper = input.temper?.replace(/\s/g, '').split(',').filter(el => el !== "");
 
     return {
         name: input.name,
