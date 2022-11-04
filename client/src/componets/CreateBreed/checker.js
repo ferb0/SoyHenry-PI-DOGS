032eaf0d97a0ceba890276a2e31f3fd141af3dda
FerB <ferb@e.email>
2022-11-04 11:50:11 -0300

    Solo cambio formato.

--- client/src/componets/CreateBreed/checker.js
+++ client/src/componets/CreateBreed/checker.js
@@ -1,15 +1,12 @@
 export default function checker(parameter, value) {
-
-    if (value && parameter === "name" && !value.match(/^[a-z ]+$/i)) {
+    // Retorna true si no es correcto
+    
+    if (value && parameter === "name" && !value.match(/^[a-z ]+$/i))
         return true;
-    }
-    else if (value && parameter === "temper" && !value.match(/^[a-z ,]+$/i)) {
+    else if (value && parameter === "temper" && !value.match(/^[a-z ,]+$/i)) 
         return true;
-    }
-    else if (value &&  (parameter.includes('min') || parameter.includes('max')) && !value.match(/^[0-9]+$/)){
+    else if (value &&  (parameter.includes('min') || parameter.includes('max')) && !value.match(/^[0-9]+$/))
         return true;
-    }
-    else {
+    else
         return false;
-    }
 };
