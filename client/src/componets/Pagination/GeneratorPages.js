d49506e778a6f09460297eecc30b539211ed43fc
ferb0 <ferb@e.email>
2022-11-05 19:54:46 -0300

    Se ocultan paginas anterior siguiente cuando no hay datos.

--- client/src/componets/Pagination/GeneratorPages.js
+++ client/src/componets/Pagination/GeneratorPages.js
@@ -2,14 +2,16 @@ import ButtonPage from './ButtonPage/ButtonPage.jsx';
 
 export function generatorPages(breedsFinal, handleClick) {
     let pages = [];
-    console.log(breedsFinal)
-    for (let i = 0; i < Math.ceil(breedsFinal[1] / 8); i++)
-        pages.push(<ButtonPage key={i + 1} handleClick={handleClick} value={i + 1} disable={i === breedsFinal[0] - 1} name={i + 1} />);
 
-    pages.unshift(<ButtonPage key={'anterioir'} handleClick={handleClick} value={breedsFinal[0] - 1}
-        disable={breedsFinal[0] === 1 || breedsFinal[1] === 0} name={'<<'} />);
-    pages.push(<ButtonPage key={'siguiente'} handleClick={handleClick} value={breedsFinal[0] + 1}
-        disable={breedsFinal[0] === Math.ceil(breedsFinal[1] / 8) || breedsFinal[1] === 0} name={'>>'} />);
+    if (breedsFinal[1] !== 0) {
+        for (let i = 0; i < Math.ceil(breedsFinal[1] / 8); i++)
+            pages.push(<ButtonPage key={i + 1} handleClick={handleClick} value={i + 1} disable={i === breedsFinal[0] - 1} name={i + 1} />);
+
+        pages.unshift(<ButtonPage key={'anterioir'} handleClick={handleClick} value={breedsFinal[0] - 1}
+            disable={breedsFinal[0] === 1 || breedsFinal[1] === 0} name={'<<'} />);
+        pages.push(<ButtonPage key={'siguiente'} handleClick={handleClick} value={breedsFinal[0] + 1}
+            disable={breedsFinal[0] === Math.ceil(breedsFinal[1] / 8) || breedsFinal[1] === 0} name={'>>'} />);
+    }
 
     return pages;
 };
