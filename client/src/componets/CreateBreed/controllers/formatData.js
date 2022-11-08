15aa96cfede123b1c459c7327b87fa62c911be8f
ferb0 <ferb@e.email>
2022-11-08 20:02:53 -0300

    Se verifica que los temperamentos sea un array.

--- client/src/componets/CreateBreed/controllers/formatData.js
+++ client/src/componets/CreateBreed/controllers/formatData.js
@@ -1,9 +1,12 @@
 export default function formatData(input) {
     // Se le da formato para enviar.
 
-    if (!input.name || !input.minHeight || !input.maxHeight || !input.minWeight || !input.maxWeight || !input.minLifeSpan || !input.maxLifeSpan || input.temper.length === 0){
+    if (!input.name || !input.minHeight || !input.maxHeight || !input.minWeight || !input.maxWeight || !input.minLifeSpan || !input.maxLifeSpan || input.temper.length === 0) {
         return false;
     }
+
+    if (!Array.isArray(input.temper))
+        return false;
 
     // Filter para quitar temper vacios
     let temper = input.temper?.replace(/\s/g, '').split(',').filter(el => el !== "");
