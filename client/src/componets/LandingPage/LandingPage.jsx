b8a07fb5005abdc028bbf9100c9e92a075f5cf66
ferb0 <ferb@e.email>
2022-11-08 18:43:51 -0300

    Se ajusta estilos en LandingPage.

--- client/src/componets/LandingPage/LandingPage.jsx
+++ client/src/componets/LandingPage/LandingPage.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 
+import s from './LandingPage.module.css';
 import paws from './images/paws.png';
 
 export default function LandingPage() {
 
     return (
-        <div>
-        <Link to='/principal'>InfoDog</Link>
-        <img src={paws} alt="paws" />
+        <div className={`${s.Landing} Global`}>
+
+        <img className={`${s.img} Global`} src={paws} alt="paws" />
+        <Link className={`${s.link} Global`} to='/principal'>InfoDog</Link>
+        
         </div>
     )
 };
