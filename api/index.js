6eb92d7d1633312ee857f92f16d3b2da5f92571b
FerB <ferb@e.email>
2022-11-09 12:31:03 -0300

    Para Vercel.

--- api/index.js
+++ api/index.js
@@ -21,8 +21,17 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
-  });
+// conn.sync({ force: false }).then(() => {
+//   server.listen(3001, () => {
+//     console.log('%s listening at 3001'); // eslint-disable-line no-console
+//   });
+// });
+
+// Para Vercel.
+conn.sync({ force: false });
+
+server.listen(3001, () => {
+  console.log('%s listening at 3001'); // eslint-disable-line no-console
 });
+
+module.exports = server;
