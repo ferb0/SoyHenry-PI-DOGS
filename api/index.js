40a9986af734d3f44d0c10da6d25736fe8a618dd
ferb0 <ferb@e.email>
2022-11-11 20:44:37 -0300

    Para deploy en render.

--- api/index.js
+++ api/index.js
@@ -19,19 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
-// conn.sync({ force: false }).then(() => {
-//   server.listen(3001, () => {
-//     console.log('%s listening at 3001'); // eslint-disable-line no-console
-//   });
-// });
-
-// Para Vercel.
-conn.sync({ force: false });
-
-server.listen(3001, () => {
-  console.log('%s listening at 3001'); // eslint-disable-line no-console
+conn.sync({ force: false }).then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server listening at ${PORT}`); // eslint-disable-line no-console
+  });
 });
-
-module.exports = server;
