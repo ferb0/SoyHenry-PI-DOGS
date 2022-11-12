e1e2019b042c114187c0d1da6a488cee4e6f865c
ferb0 <ferb@e.email>
2022-11-12 14:31:31 -0300

    Se agrega soporte para coneciones SSL para DB y así conectar a render DB.

--- api/src/db.js
+++ api/src/db.js
@@ -9,6 +9,9 @@ const {
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions: {
+    ssl: true // Para DB alojada en render.
+  },
   define: {
     freezeTableName: true
   }
