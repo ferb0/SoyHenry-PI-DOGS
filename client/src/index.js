f3a5882ef5befa4ef96ecbf9d0da2def53820189
ferb0 <ferb@e.email>
2022-11-13 12:16:57 -0300

    Se hace limpieza de archivos inecesarios y cambios pequeños en config.

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
 
-// Como es global solo se necesita importar en un archivo.
-// WebPack hace el resto.
 import './global/GlobalStyle.css';
 
-//Quitar después de crear HOME (App)
 import Principal from './componets/Principal/Principal.jsx';
 
 ReactDOM.render(
@@ -25,8 +20,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
