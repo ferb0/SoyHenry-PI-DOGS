61d3c63cf34d4eb20ba704ee8b01d7d8fe76277a
FerB <ferb@e.email>
2022-11-16 11:48:50 -0300

    Se limpia código y se usa variable global para cantidad Sumaries.

--- client/src/componets/Pagination/controllers/generatorPages.js
+++ client/src/componets/Pagination/controllers/generatorPages.js
@@ -1,16 +1,19 @@
 import ButtonPage from '../ButtonPage/ButtonPage.jsx';
 
+import { CANT_SUMMARIES } from '../../../global/CantSummaries.js';
+
 export function generatorPages(breedsFinal, handleClick) {
     let pages = [];
+    let limit = Math.ceil(breedsFinal[1] / CANT_SUMMARIES);
 
     if (breedsFinal[1] !== 0) {
-        for (let i = 0; i < Math.ceil(breedsFinal[1] / 8); i++)
+        for (let i = 0; i < limit; i++)
             pages.push(<ButtonPage key={i + 1} handleClick={handleClick} value={i + 1} disable={i === breedsFinal[0] - 1} name={i + 1} />);
 
         pages.unshift(<ButtonPage key={'anterioir'} handleClick={handleClick} value={breedsFinal[0] - 1}
-            disable={breedsFinal[0] === 1 || breedsFinal[1] === 0} name={'<<'} />);
+            disable={breedsFinal[0] === 1} name={'<<'} />);
         pages.push(<ButtonPage key={'siguiente'} handleClick={handleClick} value={breedsFinal[0] + 1}
-            disable={breedsFinal[0] === Math.ceil(breedsFinal[1] / 8) || breedsFinal[1] === 0} name={'>>'} />);
+            disable={breedsFinal[0] === limit} name={'>>'} />);
     }
 
     return pages;
