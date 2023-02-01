10bef64b7eb1496f415b4e6a58f62fdf4feaef2f
ferb0 <ferb@e.email>
2023-02-01 12:51:48 -0300

    Primera aproximación de lista de resultados.

--- client/src/MaterialUIComponents/Breeds/Breeds.jsx
+++ client/src/MaterialUIComponents/Breeds/Breeds.jsx
@@ -1,5 +1,36 @@
+import { useSelector } from 'react-redux';
+
+import { Typography, Container, Grid, Card, CardMedia, CardContent } from '@mui/material';
+
 export default function Breeds() {
+    const breeds = useSelector(state => state.breeds);
+
     return (
-        <h2>Breeds</h2>
+        <Container sx={{padding:'1rem'}}>
+            <Typography variant='h6' align='center'>
+                List of Breeds
+            </Typography>
+            <Grid container spacing={4}>
+                {breeds?.map(el =>
+                    <Grid key={el.name} item xs={12} sm={6} md={3} lg={3}>
+                        <Card
+                            sx={{ padding: '1rem' }}>
+                            <CardMedia
+                                component="img"
+                                image={el.img}
+                                alt="imageDog"
+                                sx={{ border: '1px solid #f3f6f4' }} />
+                            <CardContent>
+                                <Typography align='center' variant='h6'>
+                                    {el.name}
+                                </Typography>
+                                <Typography>
+                                    {el.temper}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>)}
+            </Grid>
+        </Container>
     )
 };
