463e1f03223524f0b87be17546fdace8673d6cd9
ferb0 <ferb@e.email>
2023-02-06 14:10:34 -0300

    Se mejora lista de temperamientos en BreedCart.

--- client/src/MaterialUIComponents/Breeds/BreedCard.jsx
+++ client/src/MaterialUIComponents/Breeds/BreedCard.jsx
@@ -1,27 +1,37 @@
-import { Typography, Grid, Card, CardMedia, CardContent } from '@mui/material';
+import { Typography, Grid, Card, CardMedia, CardContent, List, ListItem, ListItemText } from '@mui/material';
 
-export default function BreedCard({breeds}) {
+export default function BreedCard({ breeds }) {
     return (
         <Grid container spacing={4}>
-        {breeds?.map(el =>
-            <Grid item key={el.name} xs={12} sm={6} md={3} lg={3}>
-                <Card
-                    sx={{ padding: '1rem' }}>
-                    <CardMedia
-                        component="img"
-                        image={el.img}
-                        alt="imageDog"
-                        sx={{ border: '1px solid #f3f6f4' }} />
-                    <CardContent>
-                        <Typography align='center' variant='h6'>
-                            {el.name}
-                        </Typography>
+            {breeds?.map(el =>
+                <Grid item key={el.name} xs={12} sm={6} md={3} lg={3}>
+                    <Card
+                        sx={{ padding: '1rem' }}>
+                        <CardMedia
+                            component="img"
+                            image={el.img}
+                            alt="imageDog"
+                            sx={{ border: '1px solid #f3f6f4' }} />
+                        <CardContent>
+                            <Typography align='center' variant='h6'>
+                                {el.name}
+                            </Typography>
+                        
                         <Typography>
-                            {el.temper}
+                            Temperaments
                         </Typography>
-                    </CardContent>
-                </Card>
-            </Grid>)}
-    </Grid>
+                            <List dense={true}>
+                                {el.temper?.map(el => {
+                                    return (
+                                        <ListItem>
+                                            <ListItemText primary={el} />
+                                        </ListItem>
+                                    )
+                                })}
+                            </List>
+                        </CardContent>
+                    </Card>
+                </Grid>)}
+        </Grid>
     )
 };
