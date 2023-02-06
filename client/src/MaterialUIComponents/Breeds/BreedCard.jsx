2bf9c0fa0d580cd4adfb88cb4dc7942e7923041e
ferb0 <ferb@e.email>
2023-02-06 14:58:17 -0300

    Se mejora BreedCard.

--- client/src/MaterialUIComponents/Breeds/BreedCard.jsx
+++ client/src/MaterialUIComponents/Breeds/BreedCard.jsx
@@ -1,4 +1,5 @@
-import { Typography, Grid, Card, CardMedia, CardContent, List, ListItem, ListItemText } from '@mui/material';
+import { Typography, Grid, Card, CardMedia, CardContent, List, ListItem, ListItemText, Stack } from '@mui/material';
+import { Container } from '@mui/system';
 
 export default function BreedCard({ breeds }) {
     return (
@@ -13,22 +14,40 @@ export default function BreedCard({ breeds }) {
                             alt="imageDog"
                             sx={{ border: '1px solid #f3f6f4' }} />
                         <CardContent>
-                            <Typography align='center' variant='h6'>
+                            <Typography align='center' variant='h6' sx={{ paddingBottom: '0.20rem' }}>
                                 {el.name}
                             </Typography>
-                        
-                        <Typography>
-                            Temperaments
-                        </Typography>
-                            <List dense={true}>
-                                {el.temper?.map(el => {
-                                    return (
-                                        <ListItem>
-                                            <ListItemText primary={el} />
-                                        </ListItem>
-                                    )
-                                })}
-                            </List>
+                            <hr />
+                            <Stack direction='row' spacing={2}>
+                                <Typography>
+                                    Weight
+                                </Typography>
+
+                                <Typography variant='subtitle2'>
+                                    {el.weight[0]} - {el.weight[1]}
+                                </Typography>
+                            </Stack>
+
+                            <Stack>
+                                <Typography>
+                                    Temperaments
+                                </Typography>
+                                <List dense={true}>
+                                    {el.temper?.map(el => {
+                                        return (
+                                            <ListItem key={el}>
+                                                <ListItemText primary={el} />
+                                            </ListItem>
+                                        )
+                                    })}
+                                </List>
+                            </Stack>
+
+                            <Container sx={{ display: 'flex' }}>
+                                <Typography variant='caption' sx={{ display: 'flex', marginLeft: 'auto' }}>
+                                    {el.source}
+                                </Typography>
+                            </Container>
                         </CardContent>
                     </Card>
                 </Grid>)}
