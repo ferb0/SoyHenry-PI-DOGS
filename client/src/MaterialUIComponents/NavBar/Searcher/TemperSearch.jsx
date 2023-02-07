c247b2efcf0e4bad105592bb83235c624ea74494
ferb0 <ferb@e.email>
2023-02-07 09:18:54 -0300

    Se agrega value a TemperSearch para conectarlo con variable en Redux.

--- client/src/MaterialUIComponents/NavBar/Searcher/TemperSearch.jsx
+++ client/src/MaterialUIComponents/NavBar/Searcher/TemperSearch.jsx
@@ -8,6 +8,7 @@ export default function TemperSearch() {
     const dispatch = useDispatch();
 
     let tempers = useSelector(state => state.tempers);
+    let temperSelected = useSelector(state => state.temperSelected);
 
     function handleOnChangeTempers(temper) {
         dispatch(setTemper(temper));
@@ -20,7 +21,8 @@ export default function TemperSearch() {
                 options={tempers}
                 sx={{ width: 175 }}
                 renderInput={(params) => <TextField {...params} label="Temperaments" />}
-                onChange={(event, value) => handleOnChangeTempers(value)} /> :
+                onChange={(event, value) => handleOnChangeTempers(value)} 
+                value={temperSelected} /> :
                 <Typography sx={{ color: 'red' }}>
                     Error to load Temperaments.
                 </Typography>}
