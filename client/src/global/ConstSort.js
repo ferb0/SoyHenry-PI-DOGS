f4b9c5c6b26b77933b60466fd9fdea38ddb64f82
ferb0 <ferb@e.email>
2023-02-07 09:50:16 -0300

    Se cambia nombres de las constantes.

--- client/src/global/ConstSort.js
+++ client/src/global/ConstSort.js
@@ -1,4 +1,4 @@
-export const ALPHA_ASC = "ALPHA_ASC"
-export const ALPHA_DES = "ALPHA_DES";
-export const WEIGHT_ASC = "WEIGHT_ASC";
-export const WEIGHT_DES = "WEIGHT_DES";
+export const ALPHA_ASC = "Alpha Asc"
+export const ALPHA_DES = "Alpha Des";
+export const WEIGHT_ASC = "Weight Asc";
+export const WEIGHT_DES = "Weight Des";
