04cc56b6a16bf679058eaa041b5f21996b8f916b
ferb0 <ferb@e.email>
2023-02-08 14:05:00 -0300

    Se setea color borde input desde theme.

--- client/src/global/Theme.js
+++ client/src/global/Theme.js
@@ -1,4 +1,26 @@
 import { createTheme } from '@mui/material/styles';
-const theme = createTheme({});
+
+const grey = '#464646';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            50: grey,
+            100: grey,
+            200: grey,
+            300: grey,
+            400: grey,
+            500: grey,
+            600: grey,
+            700: grey,
+            800: grey,
+            900: grey,
+            A100: grey,
+            A200: grey,
+            A400: grey,
+            A700: grey,
+        }
+    },
+});
 
 export default theme;
