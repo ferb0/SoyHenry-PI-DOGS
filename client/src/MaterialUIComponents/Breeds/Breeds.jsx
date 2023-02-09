6ea8598ba828e54dde32231c4c9ba155e096f4df
ferb0 <ferb@e.email>
2023-02-09 09:18:07 -0300

    Se agrega mensaje en primera carga.

--- client/src/MaterialUIComponents/Breeds/Breeds.jsx
+++ client/src/MaterialUIComponents/Breeds/Breeds.jsx
@@ -1,29 +1,47 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 
 import { temperFilter, sourceFilter, cantFilter } from '../../controllers/Pagination/funcionsFilter.js';
 import { sortType } from '../../controllers/Pagination/functiosSort.js';
 import { CANT_SUMMARIES } from '../../global/CantSummaries.js';
+import { firstLoadingOff } from '../../redux/actions.js';
 
 import { Typography, Container, Pagination, Stack, CircularProgress } from '@mui/material';
 import BreedCard from './BreedCard.jsx';
 
 export default function Breeds() {
-    const { breeds, sourceSelected, temperSelected, sortSelected, loadingBreed } = useSelector(state => {
-        return {
-            breeds: state.breeds,
-            sourceSelected: state.filterType,
-            temperSelected: state.temperSelected,
-            sortSelected: state.sortSelected,
-            loadingBreed: state.loadingBreed
-        }
-    });
+    const dispatch = useDispatch();
+
+    const {
+        breeds,
+        tempers,
+        sourceSelected,
+        temperSelected,
+        sortSelected,
+        loadingBreed,
+        firstLoading } = useSelector(state => {
+            return {
+                breeds: state.breeds,
+                tempers: state.tempers,
+                sourceSelected: state.filterType,
+                temperSelected: state.temperSelected,
+                sortSelected: state.sortSelected,
+                loadingBreed: state.loadingBreed,
+                firstLoading: state.firstLoading
+            }
+        });
+
     let [breedsFinal, setBreedsFinal] = useState(
         [
             0, // Página actual.
             0, // Cantidad total de elementos.
             [] // Elementos a mostrar por página.
         ]);
+
+    useEffect(() => {
+        if (tempers.length !== 0)
+            dispatch(firstLoadingOff());
+    }, [tempers]);
 
     useEffect(() => {
         if (breeds !== false) {
@@ -53,41 +71,45 @@ export default function Breeds() {
             </Typography>
             {!loadingBreed ?
                 breeds === false ?
-                <Typography color='error' align='center' sx={{paddingTop: '3rem'}}>
-                    Error getting data.
-                </Typography>
-                :
-                breeds?.length !== 0 ?
-                <>
-                    <Stack align='center'>
-                        <Pagination
-                            page={breedsFinal[0]}
-                            count={Math.ceil(breedsFinal[1] / CANT_SUMMARIES)}
-                            onChange={handleClick}
-                            sx={{ margin: 'auto', paddingBottom: '1rem', paddingTop: '1rem' }} />
-                    </Stack>
+                    <Typography color='error' align='center' sx={{ paddingTop: '3rem' }}>
+                        Error getting data.
+                    </Typography>
+                    :
+                    breeds?.length !== 0 ?
+                        <>
+                            <Stack align='center'>
+                                <Pagination
+                                    page={breedsFinal[0]}
+                                    count={Math.ceil(breedsFinal[1] / CANT_SUMMARIES)}
+                                    onChange={handleClick}
+                                    sx={{ margin: 'auto', paddingBottom: '1rem', paddingTop: '1rem' }} />
+                            </Stack>
 
-                    <BreedCard breeds={breedsFinal[2]} />
+                            <BreedCard breeds={breedsFinal[2]} />
 
-                    <Stack align='center'>
-                        <Pagination
-                            page={breedsFinal[0]}
-                            count={Math.ceil(breedsFinal[1] / CANT_SUMMARIES)}
-                            onChange={handleClick}
-                            sx={{ margin: 'auto', paddingBottom: '1rem', paddingTop: '1rem' }} />
-                    </Stack>
-                </> : 
-                <Typography align='center' sx={{paddingTop: '3rem'}}>
-                    Without results.
-                </Typography>
+                            <Stack align='center'>
+                                <Pagination
+                                    page={breedsFinal[0]}
+                                    count={Math.ceil(breedsFinal[1] / CANT_SUMMARIES)}
+                                    onChange={handleClick}
+                                    sx={{ margin: 'auto', paddingBottom: '1rem', paddingTop: '1rem' }} />
+                            </Stack>
+                        </> :
+                        <Typography align='center' sx={{ paddingTop: '3rem' }}>
+                            Without results.
+                        </Typography>
                 :
-                <Container align='center' sx={{paddingTop: '3rem'}}>
+                <Container align='center' sx={{ paddingTop: '3rem' }}>
                     <CircularProgress
                         color="inherit"
                         size='3rem' />
                     <Typography>
                         Loading...
                     </Typography>
+                    {firstLoading ?
+                        <Typography component='p' variant='caption'>
+                            The first upload may be very slow due to technical characteristics of the server. Please wait.
+                        </Typography> : null}
                 </Container>}
         </Container>
     )
