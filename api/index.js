11598a378696227ef266e99bbde2293554dd22c7
ferb0 <ferb@e.email>
2023-02-13 09:34:03 -0300

    Para que se restablezca la base de datos.

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log(`Server listening at ${PORT}`); // eslint-disable-line no-console
   });
