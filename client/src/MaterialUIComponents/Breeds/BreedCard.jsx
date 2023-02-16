909db99f0948c7294a204100979be49d97c2db43
ferb0 <ferb@e.email>
2023-02-16 09:31:55 -0300

    Se agregan botones delete modify a CardBreed.

--- client/src/MaterialUIComponents/Breeds/BreedCard.jsx
+++ client/src/MaterialUIComponents/Breeds/BreedCard.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 
-import { Typography, Grid, Card, CardMedia, CardContent, List, ListItem, ListItemText, Stack, Container } from '@mui/material';
+import { Typography, Grid, Card, CardMedia, CardContent, List, ListItem, ListItemText, Stack, Container, Button } from '@mui/material';
+
+import { DB } from '../../global/ConstSource.js';
 
 import imageDefault from '../../global/images/paws.png';
 
@@ -14,58 +16,78 @@ export default function BreedCard({ breeds }) {
                     item
                     key={el.name}
                     xs={12} sm={6} md={4} lg={3}>
-                    <Link to={`/detail/${el.id}`} style={{ textDecoration: "none" }}>
-                        <Card
-                            sx={{ padding: '1rem' }}>
-
+                    <Card
+                        sx={{ padding: '1rem' }}>
+                        <Link to={`/detail/${el.id}`} style={{ textDecoration: "none" }}>
                             <CardMedia
                                 component="img"
                                 image={el.img ? el.img : imageDefault}
                                 alt="imageDog"
                                 sx={{ border: '1px solid #f3f6f4' }} />
+                        </Link>
+                        <CardContent>
+                            <Typography align='center' component='h6' variant='h6' sx={{ paddingBottom: '0.20rem' }}>
+                                {el.name}
+                            </Typography>
+                            <hr />
+                            <Stack
+                                direction='row'
+                                spacing={2}
+                                alignItems="center" >
 
-                            <CardContent>
-                                <Typography align='center' component='h6' variant='h6' sx={{ paddingBottom: '0.20rem' }}>
-                                    {el.name}
+                                <Typography variant='h6'>
+                                    Weight:
                                 </Typography>
-                                <hr />
-                                <Stack
-                                    direction='row'
-                                    spacing={2}
-                                    alignItems="center" >
 
-                                    <Typography variant='h6'>
-                                        Weight:
-                                    </Typography>
+                                <Typography component='p' variant='body1'>
+                                    {el.weight[0]}Kg - {el.weight[1]}Kg
+                                </Typography>
+                            </Stack>
 
-                                    <Typography component='p' variant='body1'>
-                                        {el.weight[0]}Kg - {el.weight[1]}Kg
-                                    </Typography>
-                                </Stack>
+                            <Stack>
+                                <Typography variant='h6'>
+                                    Temperaments:
+                                </Typography>
+                                <List dense={true} sx={{ padding: '0' }}>
+                                    {el.temper?.map(el => {
+                                        return (
+                                            <ListItem key={el} sx={{ padding: '0', paddingLeft: '1rem' }}>
+                                                <ListItemText primary={el} />
+                                            </ListItem>
+                                        )
+                                    })}
+                                </List>
+                            </Stack>
 
-                                <Stack>
-                                    <Typography variant='h6'>
-                                        Temperaments:
-                                    </Typography>
-                                    <List dense={true} sx={{ padding: '0' }}>
-                                        {el.temper?.map(el => {
-                                            return (
-                                                <ListItem key={el} sx={{ padding: '0', paddingLeft: '1rem' }}>
-                                                    <ListItemText primary={el} />
-                                                </ListItem>
-                                            )
-                                        })}
-                                    </List>
-                                </Stack>
-
-                                <Container sx={{ display: 'flex' }}>
-                                    <Typography variant="caption" sx={{ display: 'flex', marginLeft: 'auto' }}>
-                                        {el.source}
-                                    </Typography>
+                            {el.source === DB ?
+                                <Container
+                                sx={{display: 'flex', paddingTop: '2rem'}}>
+                                    <Button
+                                        variant="outlined"
+                                        size='small'
+                                        color='secondary'
+                                        sx={{ margin: 'auto' }}>
+                                        Delete
+                                    </Button>
+                                    <Button
+                                        variant="outlined"
+                                        size='small'
+                                        color='secondary'
+                                        disabled
+                                        sx={{ margin: 'auto' }}>
+                                        Modify
+                                    </Button>
                                 </Container>
-                            </CardContent>
-                        </Card>
-                    </Link>
+                                :
+                                null}
+
+                            <Container sx={{ display: 'flex', paddingTop: '1rem' }}>
+                                <Typography variant="caption" sx={{ display: 'flex', marginLeft: 'auto' }}>
+                                    {el.source}
+                                </Typography>
+                            </Container>
+                        </CardContent>
+                    </Card>
                 </Grid>)}
         </Grid>
     )
