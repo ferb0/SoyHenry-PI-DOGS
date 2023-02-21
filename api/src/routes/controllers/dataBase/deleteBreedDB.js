01b5aa7e0c298af704ee010a97c209df1da7fec2
ferb0 <ferb@e.email>
2023-02-21 12:32:59 -0300

    Se agrega funcionalidad modificar breed de la api en delete breed de la Db..

--- api/src/routes/controllers/dataBase/deleteBreedDB.js
+++ api/src/routes/controllers/dataBase/deleteBreedDB.js
@@ -1,15 +1,17 @@
 const { DogM } = require('../../../models-mongodb/Dog.js');
 const { Dog, Temper } = require('../../../db.js');
 const { TempersM } = require('../../../models-mongodb/Tempers.js');
+const { ExcludesM } = require('../../../models-mongodb/ExcludedBreeds.js');
 
 async function deleteDBM(idBreed) {
     try {
         // Se elimina dog
         await DogM.deleteOne({ _id: idBreed });
-        await TempersM.deleteMany({dogId: idBreed});
+        await TempersM.deleteMany({ dogId: idBreed });
+        await ExcludesM.deleteOne({ idDog: idBreed });
     }
     catch (error) {
-
+        throw error;
     }
 };
 
