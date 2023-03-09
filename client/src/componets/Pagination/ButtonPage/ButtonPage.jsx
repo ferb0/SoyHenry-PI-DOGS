3d41f270dd330d8a7d949047a0e169d6008a29d2
ferb0 <ferb@e.email>
2023-03-09 09:38:11 -0300

    Se eliminan componentes viejos.

--- client/src/componets/Pagination/ButtonPage/ButtonPage.jsx
+++
@@ -1,7 +0,0 @@
-import s from './ButtonPage.module.css';
-
-export default function ButtonPage(props) {
-    return (
-        <button className={`${s.button}`} value={props.value} onClick={props.handleClick} disabled={props.disable}>{props.name}</button>
-    )
-};
