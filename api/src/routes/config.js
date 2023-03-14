53f52a59bcb74dd387a5a2d2940f770d4ad4531f
ferb0 <ferb@e.email>
2023-03-14 08:47:19 -0300

    Se agrega nueva ruta.

--- api/src/routes/config.js
+++ api/src/routes/config.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const { Router } = require('express');
 const router = Router();
 
-const {getBreedsNumberDataBase} = require('./controllers/data_dase/get_breeds_number_DB.js');
+const { getBreedsNumberDataBase } = require('./controllers/data_dase/get_breeds_number_DB.js');
 
-const { NUMBER_MAX_ITEMS_DB } = process.env;
+const { NUMBER_MAX_ITEMS_DB, BREEDS_FOR_PAGE } = process.env;
 
 router.get('/breedsNumber', async (req, res) => {
     try {
@@ -19,6 +19,10 @@ router.get('/breedsNumber', async (req, res) => {
     catch (error) {
         res.status(500).json({ err: err.message });
     }
+});
+
+router.get('/breedsForPage', (req, res) => {
+    res.json({ msg: BREEDS_FOR_PAGE });
 });
 
 module.exports = router;
